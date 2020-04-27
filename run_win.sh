e5234500d833405e332b90d4cbd792cd33c6c1d5
ahmadshabib <ahmadshabib94@gmail.com>
2020-04-27 17:30:10 +0300

    feat: runnable executor

---
+++ run_win.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+JARFILE=./shopping-cart-app/target/shopping-cart-app-1.0-SNAPSHOT.jar
+
+# Build the application and generate the executable
+if  mvn -v | egrep "\S+\s+Maven"; then
+        mvn -B -T 4C clean install -Dskiptests
+    else
+        echo "Maven is not installed, please make sure to setup maven"
+fi
+
+# Check if java exists
+if java -version 2>&1 >/dev/null | egrep "\S+\s+version"; then
+    if [ -f $JARFILE ] ; then
+        java -jar $JARFILE
+    else
+        echo "File was not found"
+    fi
+    else
+        echo  "Java is not installed please install then run again"
+fi
+wait
