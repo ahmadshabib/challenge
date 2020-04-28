5539341b09614f45d169ea24e6a7c33b38228188
ahmadshabib <ahmadshabib94@gmail.com>
2020-04-28 11:28:13 +0300

    fix: github workflow fix

--- run_win.sh
+++ run_win.sh
@@ -4,7 +4,7 @@ JARFILE=./shopping-cart-app/target/shopping-cart-app-1.0-SNAPSHOT.jar
 
 # Build the application and generate the executable
 if  mvn -v | egrep "\S+\s+Maven"; then
-        mvn -B -T 4C clean install -Dskiptests
+        mvn -B -T 4C clean install -DskipTests
     else
         echo "Maven is not installed, please make sure to setup maven"
 fi
